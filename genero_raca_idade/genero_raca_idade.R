f2f8eb5d98f6b35dd23ce1e3deb8d3f6cd1cfffc
Iane Martins <ianemartins99@gmail.com>
2024-07-01 15:54:55 -0300

    atualizando documento de filtragem de dados, adicionadas duas colunas

--- genero_raca_idade/genero_raca_idade.R
+++ genero_raca_idade/genero_raca_idade.R
@@ -1,7 +1,30 @@
 library(remotes) 
 library(microdatasus) 
-library(dplyr) 
+library(dplyr)
 library(stringr) 
 library(ggplot2) 
 library(lubridate) 
+
+#ler a base de dados
+dados_br_total=c()
+for (ano in 2013:2022) {
+  
+  df <- readRDS(paste0("base_de_dados/dados_", ano,".rds"))
+  dados_br_total <- bind_rows(df,dados_br_total)
+  
+}
+
+# -------------- ANALISE GENERO -------------
+
+#1.1 quantidade de mortes por genero no brasil
+frequencia_genero_br <- dados_br_total %>%
+  group_by(SEXO) %>%
+  summarise(Quantidade = n(), .groups = "drop") %>%
+  mutate(Porcentagem = Quantidade / sum(Quantidade) * 100)
+
+#1.2 quantidade de mortes por genero no espirito santo
+#1.3 quantidade de mortes por psicoatvos por genero no brasil
+#1.4 quantidade de mortes por psicoativos por genero no espirito santo 
+
+
 
