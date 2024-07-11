bd931348a3933f7768ec99611786c7b9c903edee
Franco <franco.lovatti@gmail.com>
2024-07-11 19:10:49 -0300

    org pasta

--- carregar pacotes.R
+++
@@ -1,4 +0,0 @@
-pacman::p_load("tidyverse", "janitor", "Hmisc",
-               "summarytools", "DataExplorer", 
-               "knitr","readr", "tidymodels", "MASS",
-               "psych", "stats", "reshape2", "sidrar")
