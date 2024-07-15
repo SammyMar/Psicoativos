e92484cbb81c7cc3839e64e70064f5a296ba996b
Iane Martins <ianemartins99@gmail.com>
2024-07-15 10:27:41 -0300

    att leitursa_filtragem_02

--- ANALISE_EXPLORATORIA_1/CODIGOS/preparacao_dos_dados/leitura_filtragem_02.R
+++ ANALISE_EXPLORATORIA_1/CODIGOS/preparacao_dos_dados/leitura_filtragem_02.R
@@ -12,65 +12,63 @@ library(reactable)
 library(stringr)
 library(sidrar)
 
-  #LER E SALVAR DADOS TOTAIS BR
+#LER E SALVAR DADOS TOTAIS BR
   
-  #### SETAR O SEU CAMINHO LOCAL ATÉ A PASTA /ANÁLISE EXPLORATÓRIA 1
+#### SETAR O SEU CAMINHO LOCAL ATÉ A PASTA /ANÁLISE EXPLORATÓRIA 1
   
-  setwd("Psicoativos/ANALISE_EXPLORATORIA_1")
-  
->>>>>>> 69d8e13 (relatório_sociais)
+setwd("Psicoativos/ANALISE_EXPLORATORIA_1")
   
   
-  dados_br_total <- c()
+dados_br_total <- c()
   
-  for (ano in 2013:2022) {
+for (ano in 2013:2022) {
     
-    df <- readRDS(paste0("base_de_dados/dados_", ano,".rds"))
-    dados_br_total <- bind_rows(df,dados_br_total)
+  df <- readRDS(paste0("base_de_dados/dados_", ano,".rds"))
+  dados_br_total <- bind_rows(df,dados_br_total)
     
-  }
+}
   
-  #ADICIONANDO COLUNAS EXTRAS
+#ADICIONANDO COLUNAS EXTRAS
   
-  #criar coluna do ano em que o obito ocorreu
-  dados_br_total$DTOBITO <- ymd(dados_br_total$DTOBITO)
-  dados_br_total$ANOOBITO <- year(dados_br_total$DTOBITO)
+#criar coluna do ano em que o obito ocorreu
+dados_br_total$DTOBITO <- ymd(dados_br_total$DTOBITO)
+dados_br_total$ANOOBITO <- year(dados_br_total$DTOBITO)
   
-  #criar coluna da idade em anos completos
-  dados_br_total$DTNASC  <- ymd(dados_br_total$DTNASC)  
-  dados_br_total$DTOBITO  <- ymd(dados_br_total$DTOBITO)
-  dados_br_total$IDADE2  <- floor(interval(start  =  dados_br_total$DTNASC , 
+#criar coluna da idade em anos completos
+dados_br_total$DTNASC  <- ymd(dados_br_total$DTNASC)  
+dados_br_total$DTOBITO  <- ymd(dados_br_total$DTOBITO)
+dados_br_total$IDADE2  <- floor(interval(start  =  dados_br_total$DTNASC , 
                                   end = dados_br_total $ DTOBITO) / years(1)) # arrendondado p baixo - anos completos
   
   
   
-  #codigos UF
+#codigos UF
   
-  var_extras_UF <- read_table("bases_de_dados_externas/var_extras_UF.txt")
-  
-  
-  var_extras_UF$codigoUF <- as.character(var_extras_UF$codigoUF)
+var_extras_UF <- read_table("bases_de_dados_externas/var_extras_UF.txt")
   
   
-  # criando a var codigosUF
-  dados_br_total <- dados_br_total %>% 
-    mutate(
-      codigoUF = substr(as.character(CODMUNOCOR), 1, 2)
-    )
-  
-  dados_br_total <- inner_join(dados_br_total, var_extras_UF, by = "codigoUF")
+var_extras_UF$codigoUF <- as.character(var_extras_UF$codigoUF)
   
   
-  dados_es_total <- dados_br_total %>%
-    filter(Sigla == "ES")
+# criando a var codigosUF
+dados_br_total <- dados_br_total %>% 
+  mutate(
+    codigoUF = substr(as.character(CODMUNOCOR), 1, 2)
+)
+  
+dados_br_total <- inner_join(dados_br_total, var_extras_UF, by = "codigoUF")
   
   
-  ### BR PSIC
-  dados_br_psic <- dados_br_total[grepl("F1", dados_br_total$CAUSABAS, ignore.case = TRUE),]
+dados_es_total <- dados_br_total %>%
+  filter(Sigla == "ES")
   
-  ### ES PSIC
   
-  dados_es_psic <- dados_es_total[grepl("F1", dados_es_total$CAUSABAS, ignore.case = TRUE),]
+### BR PSIC
+dados_br_psic <- dados_br_total[grepl("F1", dados_br_total$CAUSABAS, ignore.case = TRUE),]
+  
+### ES PSIC
+  
+dados_es_psic <- dados_es_total[grepl("F1", dados_es_total$CAUSABAS, ignore.case = TRUE),]
   
   
   
