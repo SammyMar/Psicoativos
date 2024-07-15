bcd5d9abad297d523271c17d07968206cc113a66
Franco <franco.lovatti@gmail.com>
2024-07-14 21:13:46 -0300

    t

--- ANÁLISE EXPLORATÓRIA 1/CODIGOS/baixar_pop_13a21.R
+++ ANÁLISE EXPLORATÓRIA 1/CODIGOS/baixar_pop_13a21.R
@@ -14,21 +14,9 @@ pop13a21 <- pop13a21 %>% dplyr::select(codigoUF = brasil_e_unidade_da_federacao_
 #tranformar os codigos das UFs em sigla UF
 
 
-# Combinar a tabela pop13a21 com a tabela car_extras_UF
+# Combinar a tabela pop13a21 com a tabela var_extras_UF
 pop13a21 <- pop13a21 %>%
-  left_join(var_extras_UF, by = c("brasil_e_unidade_da_federacao_codigo" = "codigoUF"))
+  left_join(var_extras_UF, by = "codigoUF")
 
 print(pop13a21)
 
-# somar as populações p/ ter a pop do brasil
-total_br_pop22 <- pop22 %>% summarise(pop22 = sum(pop22))
-
-brasil_pop22 <- data.frame(
-  codigoUF = as.character (1),
-  pop22 = total_br_pop22$pop22
-)
-
-# Adicionar a nova linha à base de dados pop22
-pop22 <- pop22 %>%
-  mutate(codigoUF = as.character(codigoUF))
-pop22 <- bind_rows(pop22, brasil_pop22)
