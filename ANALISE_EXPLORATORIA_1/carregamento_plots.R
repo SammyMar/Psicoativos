a506501144b1721a79d2fdf6320d30d9699b4042
SammyMar <samuel.martins7@hotmail.com>
2024-07-17 02:17:15 -0300

    .

--- ANALISE_EXPLORATORIA_1/carregamento_plots.R
+++ ANALISE_EXPLORATORIA_1/carregamento_plots.R
@@ -11,43 +11,90 @@ load("GRAFICOS_RDA/mapa_psic_pop13.rda")
 load("GRAFICOS_RDA/mapa_psic_pop22.rda")
 load("GRAFICOS_RDA/heatmap_mortes_psic.rda")
 
-
 load("GRAFICOS_RDA/serie_indic1.rda")
 load("GRAFICOS_RDA/serie_indic2.rda")
 
 load("GRAFICOS_RDA/serie_indic2_3.rda")
 load("GRAFICOS_RDA/serie_indic4_5.rda")
 
+load("GRAFICOS_RDA/series_mortes_br_psic.rda")
+load("GRAFICOS_RDA/series_mortes_br_total.rda")
+load("GRAFICOS_RDA/series_mortes_es_total.rda")
+load("GRAFICOS_RDA/series_mortes_es_psic.rda")
 
-load("GRAFICOS_RDA/series_mortes_br_psic")
-load("GRAFICOS_RDA/series_mortes_br_total")
-load("GRAFICOS_RDA/series_mortes_es_total")
-load("GRAFICOS_RDA/series_mortes_es_psic")
+load("GRAFICOS_RDA/series_genero_br_total.rda")
+load("GRAFICOS_RDA/series_genero_es_total.rda")
+load("GRAFICOS_RDA/series_genero_br_psic.rda")
+load("GRAFICOS_RDA/series_genero_es_psic.rda")
+load("GRAFICOS_RDA/proporcao_genero_br_psic.rda")
+load("GRAFICOS_RDA/proporcao_genero_es_psic.rda")
 
+load("GRAFICOS_RDA/series_raca_br_total.rda")
+load("GRAFICOS_RDA/series_raca_es_total.rda")
+load("GRAFICOS_RDA/series_raca_br_psic.rda")
+load("GRAFICOS_RDA/series_raca_es_psic.rda")
+load("GRAFICOS_RDA/proporcao_raca_br_psic.rda")
+load("GRAFICOS_RDA/proporcao_raca_es_psic.rda")
 
-
-load("GRAFICOS_RDA/series_genero_br_total")
-load("GRAFICOS_RDA/series_genero_es_total")
-load("GRAFICOS_RDA/series_genero_br_psic")
-load("GRAFICOS_RDA/series_genero_es_psic")
-load("GRAFICOS_RDA/proporcao_genero_br_psic")
-load("GRAFICOS_RDA/proporcao_genero_es_psic")
-
-load("GRAFICOS_RDA/series_raca_br_total")
-load("GRAFICOS_RDA/series_raca_es_total")
-load("GRAFICOS_RDA/series_raca_br_psic")
-load("GRAFICOS_RDA/series_raca_es_psic")
-load("GRAFICOS_RDA/proporcao_raca_br_psic")
-load("GRAFICOS_RDA/proporcao_raca_es_psic")
-
-load("GRAFICOS_RDA/series_raca_br_total")
-load("GRAFICOS_RDA/series_raca_es_total")
-load("GRAFICOS_RDA/series_raca_br_psic")
-load("GRAFICOS_RDA/series_raca_es_psic")
-load("GRAFICOS_RDA/proporcao_raca_br_psic")
-load("GRAFICOS_RDA/proporcao_raca_es_psic")
+load("GRAFICOS_RDA/series_raca_br_total.rda")
+load("GRAFICOS_RDA/series_raca_es_total.rda")
+load("GRAFICOS_RDA/series_raca_br_psic.rda")
+load("GRAFICOS_RDA/series_raca_es_psic.rda")
+load("GRAFICOS_RDA/proporcao_raca_br_psic.rda")
+load("GRAFICOS_RDA/proporcao_raca_es_psic.rda")
 
 
+#Loads Carol -----------------------------------------------------------------
+
+load("GRAFICOS_RDA/hist.F10.es.rda")
+load("GRAFICOS_RDA/hist.F10.br.rda")
+load("GRAFICOS_RDA/hist.F17.es.rda")
+load("GRAFICOS_RDA/hist.F17.br.rda")
+
+load("GRAFICOS_RDA/series_genero_br_f10.rda")
+load("GRAFICOS_RDA/series_genero_br_f17.rda")
+load("GRAFICOS_RDA/series_genero_es_f17.rda")
+load("GRAFICOS_RDA/series_genero_es_f10.rda")
+
+load("GRAFICOS_RDA/hist_escolaridade_br_f10.rda")
+load("GRAFICOS_RDA/hist_escolaridade_es_f10.rda")
+load("GRAFICOS_RDA/hist_escolaridade_br_f17.rda")
+load("GRAFICOS_RDA/hist_escolaridade_es_f17.rda")
+
+load("GRAFICOS_RDA/series_raca_br_F10.rda")
+load("GRAFICOS_RDA/series_raca_es_F10.rda")
+load("GRAFICOS_RDA/series_raca_br_F17.rda")
+load("GRAFICOS_RDA/series_raca_br_F17.rda")
+
+load("GRAFICOS_RDA/tabela_redutiva.rda")
+
+load("GRAFICOS_RDA/graf.serie.idade.es.t.rda")
+load("GRAFICOS_RDA/graf.serie.idade.es.p.rda")
+load("GRAFICOS_RDA/graf.serie.idade.br.t.rda")
+load("GRAFICOS_RDA/graf.serie.idade.br.t.rda")
+
+load("GRAFICOS_RDA/boxplot.anoidad.es.rda")
+load("GRAFICOS_RDA/boxplot.anoidad.es.psic.rda")
+#load("GRAFICOS_RDA/boxplot.anoidad.br.rda")
+load("GRAFICOS_RDA/boxplot.anoidad.br.psic.rda")
+
+load("GRAFICOS_RDA/barplot_estciv_br.rda")
+load("GRAFICOS_RDA/barplot_estciv_br_psic.rda")
+load("GRAFICOS_RDA/barplot_estciv_es.rda")
+load("GRAFICOS_RDA/barplot_estciv_es_psic.rda")
+
+load("GRAFICOS_RDA/serie_obt_psic_br.rda")
+load("GRAFICOS_RDA/serie_obt_psic_es.rda")
+
+load("GRAFICOS_RDA/grafico_prop_estciv_es.rda")
+load("GRAFICOS_RDA/grafico_prop_estciv_br.rda")
+load("GRAFICOS_RDA/grafico_prop_estciv_es_psic.rda")
+load("GRAFICOS_RDA/grafico_prop_estciv_br_psic.rda")
+
+load("GRAFICOS_RDA/grafico_prop_idade_es.rda")
+load("GRAFICOS_RDA/grafico_prop_idade_br.rda")
+load("GRAFICOS_RDA/grafico_prop_idade_es_psic.rda")
+load("GRAFICOS_RDA/grafico_prop_idade_br_psic.rda")
 
 
 
