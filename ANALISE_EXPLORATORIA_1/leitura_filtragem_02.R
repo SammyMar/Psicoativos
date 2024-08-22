de34048290b6ff2a64c0c7d37f615ede85e5849a
Franco <franco.lovatti@gmail.com>
2024-08-22 13:57:48 -0300

    .

--- ANALISE_EXPLORATORIA_1/leitura_filtragem_02.R
+++ ANALISE_EXPLORATORIA_1/leitura_filtragem_02.R
@@ -34,7 +34,7 @@
       
     for (ano in 2013:2022) {
         
-      df <- readRDS(paste0("base_de_dados/dados_", ano,".rds"))
+      df <- readRDS(paste0("bases_de_dados/dados_", ano,".rds"))
       dados_br_total <- bind_rows(df,dados_br_total)
         
     }
@@ -81,6 +81,8 @@
       
     dados_es_psic <- dados_es_total[grepl("F1", dados_es_total$CAUSABAS, ignore.case = TRUE),]
       
+    
+    
     
     
       
